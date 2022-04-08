d9d17f3aadb2d8a8e510928a5abb8a008f2eac90
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-04-08 14:42:12 +0100

    main App.js/Game setup with board component imported

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
+import Board from './components/Board';
 
 // TASK LIST
 // 1 - Break down the problem
-//   1.1 - Break down the tic tac toe game in to components - game.App.js, board, squares
-//   1.2 - Define functions and core logic for each component - game state, boardstate, square state, checkWinner, playerTurn, dipslayWinner, restartGame, renderBoard by mapping through squares x9
+//   1.1 - Break down the tic tac toe game in to components - game.App.js, board (3x3 grid), squares
+//   1.2 - Define functions and core logic for each component - game state, boardstate, square state, checkWinner, playerTurn, dipslayWinner, restartGame, renderBoard by mapping through squares x9, handleClick when square clicked
 // 2 - Universal CSS & CSS modules for each component
 // 3 - Test functionality 
 // 4 - Strecth Goals & additonal features 
 
 const App = () => {
+
+  const checkWinner = () => {
+
+  }
+
   return (
-    <div className="App">
-     
-    </div>
+    <main>
+     <Board />
+    </main>
   );
 }
 
