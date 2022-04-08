5bc6b06c22471fed7aa8830bc2da4409863ccd10
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-04-08 11:17:03 +0100

    components folder created with Board & Squares components defined

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 // TASK LIST
 // 1 - Break down the problem
-//   1.1 - Break down the tic tac toe game in to components - game, board, squares
+//   1.1 - Break down the tic tac toe game in to components - game.App.js, board, squares
 //   1.2 - Define functions and core logic for each component - game state, boardstate, square state, checkWinner, playerTurn, dipslayWinner, restartGame, renderBoard by mapping through squares x9
 // 2 - Universal CSS & CSS modules for each component
 // 3 - Test functionality 
