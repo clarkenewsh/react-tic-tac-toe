f91586c785f7c7d2fdfd70b5558f74027343b985
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-04-09 16:50:39 +0100

    squares value array setup & passed to board with props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Board from './components/Board';
 // TASK LIST
 // 1 - Break down the problem
 //   1.1 - Break down the tic tac toe game in to components - game.App.js, board (3x3 grid), squares
-//   1.2 - Define functions and core logic for each component - game state, boardstate, square state, checkWinner, playerTurn, dipslayWinner, restartGame, renderBoard by mapping through squares x9, handleClick when square clicked
+//   1.2 - Define functions and core logic for each component - game state, boardstate, square state & value, checkWinner, playerTurn, dipslayWinner, restartGame, renderBoard by mapping through squares x9 with value, handleClick when square clicked, check if winner & check square is already occupied
 // 2 - Universal CSS & CSS modules for each component
 // 3 - Test functionality 
-// 4 - Strecth Goals & additonal features 
+// 4 - Stretch Goals & additional features 
 
 const App = () => {
 
@@ -15,9 +15,20 @@ const App = () => {
 
   }
 
+  const handleClick = () => {
+    console.log("clicked");
+  }
+
+  // Set up value of each squares
+  const squares = [
+    'O', null, 'X',
+    'X', 'X', 'O',
+    'O', null, null,
+  ]
+
   return (
     <main>
-     <Board />
+     <Board checkWinner={checkWinner} handleClick={handleClick} squares={squares}/>
     </main>
   );
 }
