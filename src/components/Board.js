a7a66194d75af30aa3c9582acb5a6075f5afe1a8
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-04-09 16:42:12 +0100

    Catch sqaures arrays with priops & map through squares values in to the Square component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import Square from './Square';
+import { useState } from 'react';
 import cssClasses from './Board.module.css';
 
-const Board = () => {
+const Board = ({ squares, handleClick }) => {
 
-
-
-  const handleClick = () => {
-    console.log("clicked");
-  }
 
   return (
     <div className={cssClasses.gameBoard}>
-      <Square value='1' handleClick={handleClick} />
-      <Square value='2' handleClick={handleClick} />
-      <Square value='3' handleClick={handleClick} />
-      <Square value='4' handleClick={handleClick} />
-      <Square value='5' handleClick={handleClick} />
-      <Square value='6' handleClick={handleClick} />
-      <Square value='7' handleClick={handleClick} />
-      <Square value='8' handleClick={handleClick} />
-      <Square value='9' handleClick={handleClick} />
+      {squares.map((square, index) => (
+        <Square value={square} key={index} handleClick={handleClick} />
+      ))}
     </div>
   )
 }
